40122b80c0df126e770f345b70b472c7ad02fe82
Steve Roth <steve@rothskeller.net>
2025-08-26 03:41:26 -0700

    Import default field appearances

--- update.go
+++ update.go
@@ -245,7 +245,10 @@ func (imp *importer) importPage(pagenum int, srcPageC, destPageC *Cursor) (err e
 	if err = imp.importContentStreams(formName, srcPageC, resources, destPageC, srcBBox); err != nil {
 		return err
 	}
-	return imp.dest.AddPageContent(pagenum, fmt.Sprintf("q 0 J 1 w 0 j 0 G 0 g %s Do Q", EncodeName(formName)))
+	if err = imp.dest.AddPageContent(pagenum, fmt.Sprintf("q 0 J 1 w 0 j 0 G 0 g %s Do Q", EncodeName(formName))); err != nil {
+		return err
+	}
+	return imp.importDefaultFields(pagenum, srcPageC, destPageC)
 }
 
 func (imp *importer) importResources(srcPageC *Cursor) (res Dict, err error) {
@@ -368,4 +371,113 @@ func (imp *importer) importContentStreams(formName Name, srcPageC *Cursor, resou
 		destC.Key("XObject")
 	}
 	return destC.SetKey(formName, outref)
+}
+
+// importDefaultFields locates any checkbox or radio button fields, finds the
+// appearance dictionary for their "off" state, and adds that to the destination
+// page.
+func (imp *importer) importDefaultFields(pagenum int, srcPageC, destPageC *Cursor) (err error) {
+	var (
+		srcC   *Cursor
+		annots Array
+	)
+	srcC = srcPageC.Clone()
+	if annots, err = srcC.Key("Annots").Array(); err != nil {
+		return nil // probably means there are none
+	}
+	for i := range annots {
+		if err = imp.importAnnotation(srcC.Clone().Index(i), destPageC, pagenum, i); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// importAnnotation imports a single annotation into the destination, if it's
+// one we want.
+func (imp *importer) importAnnotation(annotC, destPageC *Cursor, pagenum, annotIdx int) (err error) {
+	var (
+		annot   Dict
+		appC    *Cursor
+		appS    Stream
+		rect    Array
+		x, y    float64
+		name    Name
+		out     Object
+		outref  Reference
+		destC   *Cursor
+		destRes Dict
+	)
+	if annot, err = annotC.Dict(); err != nil {
+		return err
+	}
+	if annot["Type"] != Name("Annot") || annot["Subtype"] != Name("Widget") {
+		return nil // not one we're interested in
+	}
+	appC = annotC.Clone()
+	if appS, err = appC.Key("AP").Key("N").Key("Off").Stream(); err != nil {
+		return nil // not one we're interested in
+	}
+	if ft, ff := findFieldDict(annotC); ft != "Btn" || ff&0x10000 != 0 {
+		return nil // not one we're interested in
+	}
+	if rect, err = annotC.Key("Rect").Array(); err != nil || len(rect) != 4 {
+		return nil // ill-formed, but we'll just ignore it
+	}
+	x, y = toFloat64(rect[0]), toFloat64(rect[1])
+	// This is an annotation that we want to add to the destination page.
+	name = Name(fmt.Sprintf("A%d", annotIdx))
+	if out, err = imp.importDeepObject(appS); err != nil {
+		return err
+	}
+	outref = imp.dest.CreateObject(out)
+	destC = destPageC.Clone().Key("Resources")
+	if destRes, err = destC.Dict(); err != nil {
+		return err
+	}
+	if _, ok := destRes["XObject"]; !ok {
+		destC.SetKey("XObject", Dict{})
+	} else {
+		destC.Key("XObject")
+	}
+	if err = destC.SetKey(name, outref); err != nil {
+		return err
+	}
+	// Add content to the destination page to display the annotation.
+	return imp.dest.AddPageContent(pagenum,
+		fmt.Sprintf("q 0 J 1 w 0 j 0 G 0 g 1 0 0 1 %.2f %.2f cm %s Do Q",
+			-x, -y, EncodeName(name)))
+}
+
+func findFieldDict(annotC *Cursor) (ft Name, ff int) {
+	var (
+		d      Dict
+		seenFf bool
+		err    error
+		c      = annotC.Clone()
+	)
+	for {
+		if d, err = c.Dict(); err != nil {
+			return "", 0
+		}
+		if _, ok := d["Ff"]; ok && !seenFf {
+			ff, _ = d["Ff"].(int)
+			seenFf = true
+		}
+		if ft, ok := d["FT"].(Name); ok {
+			return ft, ff
+		}
+		c = c.Key("Parent")
+	}
+}
+
+func toFloat64(o Object) float64 {
+	switch o := o.(type) {
+	case float64:
+		return o
+	case int:
+		return float64(o)
+	default:
+		return 0 // don't want to panic on invalid PDF
+	}
 }
