98369913caafe4cfd0b56cb80e324d6b2ac97e46
Steve Roth <steve@rothskeller.net>
2025-10-01 12:54:04 -0700

    Add matrix support.

--- pdf.go
+++ pdf.go
@@ -1,7 +1,10 @@
 // Package pdf provides methods for reading, creating, and updating PDF files.
 package pdf
 
-import "io"
+import (
+	"errors"
+	"io"
+)
 
 // An Object is an object as defined by the PDF specification.  While an Object
 // is defined as "any", it will in fact be one of the following:
@@ -71,3 +74,48 @@ type Point struct{ X, Y float64 }
 
 // PointXY returns a Point.
 func PointXY(x, y float64) Point { return Point{x, y} }
+
+// A Matrix specifies a coordinate transformation matrix.  Following PDF
+// standard, the matrix
+//
+//	┌a b 0┐
+//	│c d 0│
+//	└e f 1┘
+//
+// is represented as the array [a b c d e f].
+type Matrix struct{ A, B, C, D, E, F float64 }
+
+func (m Matrix) ToArray() Array { return Array{m.A, m.B, m.C, m.D, m.E, m.F} }
+
+func (a Array) ToMatrix() (m Matrix, err error) {
+	if len(a) != 6 {
+		return m, errors.New("ill-formed matrix")
+	}
+	for i, v := range a {
+		switch v := v.(type) {
+		case float64: // ok
+		case int:
+			a[i] = float64(v)
+		default:
+			return m, errors.New("ill-formed matrix")
+		}
+	}
+	return Matrix{a[0].(float64), a[1].(float64), a[2].(float64), a[3].(float64), a[4].(float64), a[5].(float64)}, nil
+}
+
+func (m Matrix) Multiply(o Matrix) (p Matrix) {
+	p.A = m.A*o.A + m.B*o.C
+	p.B = m.A*o.B + m.B*o.D
+	p.C = m.C*o.A + m.D*o.C
+	p.D = m.C*o.B + m.D*o.D
+	p.E = m.E*o.A + m.F*o.C + o.E
+	p.F = m.E*o.B + m.F*o.D + o.F
+	return p
+}
+func (m Matrix) PreMultiply(o Matrix) Matrix { return o.Multiply(m) }
+
+func (m Matrix) Transform(p Point) (t Point) {
+	t.X = m.A*p.X + m.C*p.Y + m.E
+	t.Y = m.B*p.X + m.D*p.Y + m.F
+	return t
+}
